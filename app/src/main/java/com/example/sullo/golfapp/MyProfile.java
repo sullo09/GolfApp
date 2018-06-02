213bba6e7125edd7f13e124750b0a1d757a27538
sullo1983@yahoo.co.uk <Greystones09>
2018-06-02 10:47:47 +0100

    saturday 2nd june

--- app/src/main/java/com/example/sullo/golfapp/MyProfile.java
+++ app/src/main/java/com/example/sullo/golfapp/MyProfile.java
@@ -1,6 +1,8 @@
 package com.example.sullo.golfapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,10 +11,14 @@ import android.widget.EditText;
 
 public class MyProfile extends AppCompatActivity {
 
+//shared preferences
+    private SharedPreferences mPreferances;
+    private SharedPreferences.Editor mEditor;
+
 
     private EditText profileFirstName;
-    private EditText ProfileEndHandicap;
-    private EditText ProfileStartHandicap;
+    private EditText profileEndHandicap;
+    private EditText profileStartHandicap;
     private EditText profileLastName;
     private Button saveProfile;
 
@@ -21,18 +27,50 @@ public class MyProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
 
+        mPreferances = getPreferences(MODE_PRIVATE);
+
         profileFirstName = (EditText) findViewById(R.id.profileFirstName);
-        ProfileEndHandicap = (EditText) findViewById(R.id.ProfileEndHandicap);
-        ProfileStartHandicap = (EditText) findViewById(R.id.ProfileStartHandicap);
+        profileEndHandicap = (EditText) findViewById(R.id.ProfileEndHandicap);
+        profileStartHandicap = (EditText) findViewById(R.id.ProfileStartHandicap);
         profileLastName = (EditText) findViewById(R.id.profileLastName);
         saveProfile = (Button) findViewById(R.id.saveProfile);
+
+        readPreferences();
+
+//saving info with shared preferences
+
 
         saveProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String firstName = profileFirstName.getText().toString();
+                String lastName = profileLastName.getText().toString();
+                String startHand = profileStartHandicap.getText().toString();
+                String endHand = profileEndHandicap.getText().toString();
+
+                SharedPreferences.Editor editor = mPreferances.edit();
+                editor.putString("keyfname", firstName);
+                editor.putString("keysurname", lastName);
+                editor.putString("keystartH", startHand);
+                editor.putString("keyendH", endHand);
+                editor.commit();
+
                 Intent BackToMain = new Intent(MyProfile.this, MainScreen.class);
                 startActivity(BackToMain);
             }
         });
     }
+
+// read and display data
+    public void readPreferences(){
+        String sname = mPreferances.getString("keyfname", "");
+        profileFirstName.setText(sname);
+        String ssurname = mPreferances.getString("keysurname", "");
+        profileLastName.setText(ssurname);
+        String sstart = mPreferances.getString("keystartH", "");
+        profileStartHandicap.setText(sstart);
+        String send = mPreferances.getString("keyendH", "");
+        profileEndHandicap.setText(send);
+    }
+
 }
